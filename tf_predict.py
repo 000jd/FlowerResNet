28474debe551f689d3bcd58cf02dc481159195ea
joydip <joydipd793@gmail.com>
2023-07-11 16:06:08 +0530

    may be

--- tf_predict.py
+++ tf_predict.py
@@ -1,14 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import numpy as np
 from utils import classes
-import cv2
 
 def tf_predict_flower(image):
     image = np.array(image)  # Convert image to a NumPy array
-    image = cv2.resize(image, (224, 224))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = np.expand_dims(image, axis=0)
-    image = tf.cast(image/255., tf.float32)
+    image = tf.image.resize(image, (224, 224))
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.expand_dims(image, axis=0)
 
     model = tf.keras.models.load_model('./models/model_L_0.17vl_0.52.h5')
 
