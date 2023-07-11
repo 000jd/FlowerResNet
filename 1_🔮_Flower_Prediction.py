90b9055c8ae010a12b0d44fcc8899545aa4e6f5e
joydip <joydipd793@gmail.com>
2023-07-11 11:11:38 +0530

    addeing changes

--- 1_🔮_Flower_Prediction.py
+++ 1_🔮_Flower_Prediction.py
@@ -6,6 +6,7 @@ from PIL import Image
 from predict import predict_flower
 from streamlit_lottie import st_lottie
 
+
 def main():
     st.set_page_config(
         page_title="FlowerResNet",
@@ -22,7 +23,8 @@ def main():
 
     # Lottie Animation
     with col1:
-        st_lottie("https://assets5.lottiefiles.com/packages/lf20_Yz2AzPLPXW.json", width=170, height=170)
+        st_lottie(
+            "https://assets5.lottiefiles.com/packages/lf20_Yz2AzPLPXW.json", width=170, height=170)
 
     # Title
     with col2:
@@ -30,7 +32,8 @@ def main():
 
     # Create language selection in the sidebar
     st.sidebar.title('Choose Language')
-    language = st.sidebar.selectbox('Select Language:', ('English', 'Spanish', 'French'))
+    language = st.sidebar.selectbox(
+        'Select Language:', ('English', 'Spanish', 'French'))
 
     # Create sidebar
     st.sidebar.info(
@@ -41,41 +44,43 @@ def main():
 
     # Image Source Selector
     with col3:
-        st.write('Choose the source of the image for prediction.', style='color: red')
+        st.write('Choose the source of the image for prediction.')
         image_source = st.radio('Select Image Source:', ('Upload', 'URL'))
 
     # Image Uploader
     with col4:
         if image_source == 'Upload':
-            uploaded_file = st.file_uploader('Choose an image file', type=['jpg', 'jpeg', 'png'])
+            uploaded_file = st.file_uploader(
+                'Choose an image file', type=['jpg', 'jpeg', 'png'])
 
-            if uploaded_file is not None:
-                image = Image.open(uploaded_file)
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file)
 
-                st.image(image, caption='Uploaded Image',
+        st.image(image, caption='Uploaded Image',
+                 use_column_width=True, width=300)
+
+        # Predict button
+        if st.button('Predict'):
+            flower_name = predict_flower(image)
+            st.success(f'Predicted Flower: {flower_name}')
+
+    elif image_source == 'URL':
+        image_url = st.text_input('Enter Image URL:')
+
+        if st.button('Predict'):
+            try:
+                response = requests.get(image_url)
+                image = Image.open(BytesIO(response.content))
+
+                st.image(image, caption='Image from URL',
                          use_column_width=True, width=300)
 
-                # Predict button
-                if st.button('Predict'):
-                    flower_name = predict_flower(image)
-                    st.success(f'Predicted Flower: {flower_name}')
+                flower_name = predict_flower(image)
+                st.success(f'Predicted Flower: {flower_name}')
 
-        elif image_source == 'URL':
-            image_url = st.text_input('Enter Image URL:')
+            except Exception as e:
+                st.error(f"Error: {e}")
 
-            if st.button('Predict'):
-                try:
-                    response = requests.get(image_url)
-                    image = Image.open(BytesIO(response.content))
-
-                    st.image(image, caption='Image from URL',
-                             use_column_width=True, width=300)
-
-                    flower_name = predict_flower(image)
-                    st.success(f'Predicted Flower: {flower_name}')
-
-                except Exception as e:
-                    st.error(f"Error: {e}")
 
 if __name__ == '__main__':
     main()
