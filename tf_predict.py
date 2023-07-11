0b882220b6ea6a2f3c9fb918ae66b02c4a19b1eb
cion247 <xocionrocks247@gmail.com>
2023-07-11 19:08:20 +0530

    test

--- tf_predict.py
+++ tf_predict.py
@@ -3,13 +3,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 from utils import classes
 
+
 def tf_predict_flower(image):
     image = np.array(image)  # Convert image to a NumPy array
     image = tf.image.resize(image, (224, 224))
     image = tf.image.convert_image_dtype(image, tf.float32)
     image = tf.expand_dims(image, axis=0)
 
-    model = tf.keras.models.load_model('FlowerResNet/models/model_L_0.17vl_0.52.h5')
+    model = tf.keras.models.load_model('model_L_0.17vl_0.52.h5')
 
     pred = model.predict(image)
     output_class = classes.class_names[np.argmax(pred)]
