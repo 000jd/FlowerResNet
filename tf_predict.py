566374e195dd829a8ea40a0d2d5c2c680adc1bab
Joydip Das <48018188+000jd@users.noreply.github.com>
2023-07-11 17:27:19 +0530

    Update tf_predict.py

--- tf_predict.py
+++ tf_predict.py
@@ -9,7 +9,7 @@ def tf_predict_flower(image):
     image = tf.image.convert_image_dtype(image, tf.float32)
     image = tf.expand_dims(image, axis=0)
 
-    model = tf.keras.models.load_model('./models/model_L_0.17vl_0.52.h5')
+    model = tf.keras.models.load_model('models/model_L_0.17vl_0.52.h5')
 
     pred = model.predict(image)
     output_class = classes.class_names[np.argmax(pred)]
